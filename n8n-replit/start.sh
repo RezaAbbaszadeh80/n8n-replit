1e932d9e463e9a70ace30ad5604ccc9b731f3bef
RezaAbbaszadeh80 <134479740+RezaAbbaszadeh80@users.noreply.github.com>
2025-07-16 23:35:01 +0330

    Update start.sh

--- n8n-replit/start.sh
+++ n8n-replit/start.sh
@@ -1,1 +1,16 @@
-1
+#!/bin/bash
+
+export N8N_BASIC_AUTH_ACTIVE=true
+export N8N_BASIC_AUTH_USER=abaszadereza41@gmail.com
+export N8N_BASIC_AUTH_PASSWORD=Hsra1234@
+export DB_TYPE=sqlite
+
+export PORT=3000
+export N8N_PORT=$PORT
+
+export WEBHOOK_URL=https://n8n-replit-abaszadereza41.repl.co
+
+# export WEBHOOK_URL=https://5a646b56-90af-4ea0-bd3a-84c542ae3a88-00-2841n6qrygut1.picard.repl.co/
+
+node ./node_modules/n8n/bin/n8n
+npx n8n
