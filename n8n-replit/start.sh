5b6f796e580bdf140cce5cc8e401e7a505461651
RezaAbbaszadeh80 <134479740+RezaAbbaszadeh80@users.noreply.github.com>
2025-07-16 23:47:17 +0330

    Update start.sh

--- n8n-replit/start.sh
+++ n8n-replit/start.sh
@@ -10,7 +10,7 @@ export N8N_PORT=$PORT
 
 export WEBHOOK_URL=https://n8n-replit-abaszadereza41.repl.co
 
-# export WEBHOOK_URL=https://5a646b56-90af-4ea0-bd3a-84c542ae3a88-00-2841n6qrygut1.picard.repl.co/
+export WEBHOOK_URL=https://5a646b56-90af-4ea0-bd3a-84c542ae3a88-00-2841n6qrygut1.picard.repl.co/
 
 node ./node_modules/n8n/bin/n8n
 npx n8n
